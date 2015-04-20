04003c4da7b3b238a4b6e482f5eb6648f982002c
manojguptagit <chintuischatting@gmail.com>
2015-04-20 10:15:04 +0530

    lots of changes

---
+++ src/test/java/org/home/expenses/test/service/BaseServiceTest.java
@@ -0,0 +1,43 @@
+/***************************************************************
+ * Project Name: ExpenseManager
+ * 
+ * File Name: BaseServiceTest.java
+ * 
+ * Author: Manoj Kumar Gupta, Created on 20 Apr 2015 at 10:03:16
+ * 
+ * *************************************************************
+ * 2015 Barclays Capital
+ * *************************************************************/
+
+package org.home.expenses.test.service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author guptam22
+ * 
+ */
+public class BaseServiceTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+
+	}
+
+}
